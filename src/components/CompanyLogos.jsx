b221e12fea3fe8cbfef9a521915d5a99dff12cca
Harshjariwala <harshjariwala34.hj@gmail.com>
2025-10-01 12:24:15 +0530

    Daily fixes

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -3,7 +3,7 @@ import data from "../data/sitedata";
 export default function CompanyLogos(){
   return (
     <section className="logos-section">
-      <div className="container logo-row">
+      <div className="logo-row">
         {data.logos.map((src,i)=> <img key={i} src={src} alt="logo" />)}
       </div>
     </section>
