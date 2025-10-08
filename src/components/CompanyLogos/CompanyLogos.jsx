ad246d8fd6896b22cc8d1804e05ff93590d3e3cd
Harshjariwala <harshjariwala34.hj@gmail.com>
2025-10-08 11:21:46 +0530

    daily fixes

--- src/components/CompanyLogos/CompanyLogos.jsx
+++ src/components/CompanyLogos/CompanyLogos.jsx
@@ -1,17 +1,48 @@
 import data from "../../data/siteData";
 import styles from "./CompanyLogos.module.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
 export default function CompanyLogos() {
   return (
     <div className={styles.logosSection} id="client-logos">
       <div className={styles.marquee}>
         <div className={styles.marqueeGroup}>
-          {data.logos.map((src, i) => (
-            <img key={`logo1-${i}`} src={src} alt={`logo-${i}`} />
-          ))}
-          {data.logos.map((src, i) => (
-            <img key={`logo2-${i}`} src={src} alt={`logo-dup-${i}`} />
-          ))}
+            <Swiper
+              modules={[Autoplay]}
+              slidesPerView={5}
+              spaceBetween={22}
+              loop={true}
+              grabCursor={true}
+              speed={2000}
+              autoplay={{
+                delay: 1000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+              }}
+              breakpoints={{
+              310: {
+                slidesPerView: 2
+              },
+              768: {
+                slidesPerView: 3
+              },
+              1024: {
+                slidesPerView: 5
+              },
+              1600: {
+                slidesPerView: 7
+              },
+        }}
+            >
+              {data.logos.map((src, i) => (
+              <SwiperSlide key={i} className={styles.cardSlide}>
+                <img key={`logo1-${i}`} src={src} alt={`logo-${i}`} />
+              </SwiperSlide>
+              ))}
+        </Swiper>
         </div>
       </div>
     </div>
