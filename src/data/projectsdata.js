b2305c93d2be6ed5a252e86e2d7eceb0d3e7f7d7
Writwik-work <Writwik.work@gmail.com>
2026-07-23 21:51:43 +0530

    Img check 3

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -48,8 +48,8 @@ const projectsData = {
       type: "A/B Testing",
       image: "/assets/project/Rapidradios.webp",
       mainImage: "/assets/project/Rapidradios.webp",
-      beforeImage: "/assets/project/beforeRapidradio.png",
-      afterImage: "/assets/project/afterRapidradio.png",
+      beforeImage: "/assets/project/beforerapidradio.png",
+      afterImage: "/assets/project/afterrapidradio.png",
       link: "https://rapidradios.com/",
     },
     {
